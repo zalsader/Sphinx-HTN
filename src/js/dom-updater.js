640993046ae293aaa77d793ec023cc44927b2975
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2018-09-16 01:20:18 -0400

    dom-updater part 1

--- src/js/dom-updater.js
+++ src/js/dom-updater.js
@@ -1,6 +1,37 @@
-function swapInChallenge() {
-
+function getChallengeTemplate() {
+  return `<div class="content">
+            <p class="topTextBold text-center">The meaning of life</p>
+            <p class="topTextBold"></p>
+          </div>
+          
+          <div class="bottomContent">
+            <button class="btnBottom"><span id="btnBottomText">Submit Answer</span></button>
+          </div>
+          `
 }
 
-alert("hello");
-export { swapInChallenge };
+function getVictoryTemplate() {
+  return `<div class="content">
+            <h1 class="topTextBold text-center">VICTORY</h1>
+          </div>
+
+        <div class="bottomContent">
+          <button class="btnBottom"><span id="btnBottomText">OH YEAHHHHH</span></button>
+        </div>
+        `
+}
+
+function swapInChallenge() {
+  // changes the html
+  const container = document.getElementById("content-wrapper");
+  container.innerHTML= getChallengeTemplate();
+}
+
+function swapInVictory() {
+  const container = document.getElementById("content-wrapper");
+  container.innerHTML = getVictoryTemplate();
+}
+
+swapInChallenge();
+setTimeout(swapInVictory, 10000);
+
